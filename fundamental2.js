4d84c01461dc5956fc16615d8f63456817188233
tran-tuong <tranvantuong2k3@gmail.com>
2022-02-08 11:38:03 +0700

    new update

--- fundamental2.js
+++ fundamental2.js
@@ -280,3 +280,130 @@ GOOD LUCK 😀
 //     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is lower than ${john.fullName}'s BMI (${john.bmi})`);
 // }
 
+
+/* The for loop 
+for(let rep = 1; rep <= 10; rep++) {
+    console.log(`Repitiotion ${rep}`);
+}
+*/
+
+/* Looping array, breaking and continuing
+const years = [1991, 2007, 1969, 2003];
+const ages = [];
+
+for(let i = 0; i < years.length; i++) {
+    ages.push(2022 - years[i]);
+}
+console.log(ages);
+
+//break and continue
+
+const tuongArray = [
+    'Tuong',
+    'Tran',
+    2022 - 2003,
+    'student',
+    ['Duy', 'Nam', 'Tu'],
+    true,
+];
+
+for(let i = 0; i < tuongArray.length; i++) {
+
+    if(typeof tuongArray[i] !== "string") continue; //skip values isn't string
+    console.log(tuongArray[i], typeof tuongArray[i]);
+}
+
+console.log('BREAK')
+for(let i = 0; i < tuongArray.length; i++) {
+
+    if(typeof tuongArray[i] !== "string") break; //stop at values isn't string
+    console.log(tuongArray[i], typeof tuongArray[i]);
+}
+
+*/
+
+/* Looping backwards and loops in loops 
+//Looping backwards
+const num = [
+    'one',
+    'two',
+    'three',
+];
+
+for(let i = num.length - 1; i >=0; i--) {
+    console.log(i, num[i]);
+}
+
+//loops in loops 
+for(let exercise = 1; exercise < 4; exercise++) {
+    console.log(`----------Start ex ${exercise}`);
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Ex ${exercise}: repition ${rep} 🏆🏆🏆`);
+    }
+}
+*/
+
+/* While loop 
+let rep = 1;
+while (rep <= 10) {
+    i++;
+    console.log('Yah 😊😊😊😊' + rep);  
+}
+
+*/
+
+//coding challege #4
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays 😉
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! Here is how to
+solve it:
+4.1. First, you will need to add up all values in the array. To do the addition,
+start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop. In each iteration, add the current value to the
+'sum' variable. This way, by the end of the loop, you have all values
+added together
+4.2. To calculate the average, divide the sum you calculated before by the
+length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+GOOD LUCK 😀
+*/
+
+const bills = [
+    22, 295, 176, 440, 37, 105, 10, 1100, 86, 52
+];
+const tips = [];
+const totals = [];
+
+const calcTip = (bill) => {
+    let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+    return tip;
+};
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
+    return sum / arr.length;
+}
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
